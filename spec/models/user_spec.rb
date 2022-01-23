6fcd86dab416d52da9263dbb47e0baa9dda474db
Jesse Nguyen <nguyenjessev@gmail.com>
2022-01-23 12:16:49 -0500

    Write User tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(email: 'test@test.com',
+                        password: 'password123',
+                        display_name: 'John Doe')
+  }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is invalid without email' do
+    subject.email = nil
+    expect(subject).to be_invalid
+  end
+
+  it 'is invalid without password' do
+    subject.password = nil
+    expect(subject).to be_invalid
+  end
+
+  it 'is invalid without display name' do
+    subject.display_name = nil
+    expect(subject).to be_invalid
+  end
 end
